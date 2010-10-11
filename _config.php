28e3ddf814f3f52012362e85c1950bc79b975c66
Daniel Suzan <daniel.suzan@gmail.com>
2010-10-11 06:04:19 -0500

    RC 1.0

--- _config.php
+++ _config.php
@@ -16,8 +16,10 @@ Director::addRules(100, array(
 
 SocializerAdmin::set_InviteFriendEmail(true);
 SocializerAdmin::set_PrintPage(true);
-SocializerAdmin::set_LibraryUse('JQuery'); //JQuery  MT  Prototype
-SocializerAdmin::set_SimpleSocializer(false);
+SocializerAdmin::set_SocializerTheme('wpzoom');
+SocializerAdmin::set_SocializerThemeSize('32px');
+SocializerAdmin::set_LibraryUse('MT'); //JQuery  MT  Prototype
+SocializerAdmin::set_SimpleSocializer(true);
 
 
 if (SocializerAdmin::get_InviteFriendEmail()) {
